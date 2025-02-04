c982e75fd4b8e2ef3be950c7c2913c5b99ed9b46
hachadorian <hachadorian98@gmail.com>
2025-02-04 16:40:39 -0500

    v3 endpoint

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import path from "path";
 import express from "express";
 import createAssessment from "./recaptcha";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(
+  cors({
+    origin: ["https://www.dvrpc.org/", "https://alpha.dvrpc.org/"],
+  }),
+);
 
 app.use(express.json());
 
@@ -20,6 +27,19 @@ router.post("/", async (req, res) => {
     console.error(error);
     res.status(500).json({ error });
   }
+});
+
+router.post("/v3", async (req, res) => {
+  const secret = process.env.SITESECRET;
+  const token = req.body.token;
+  const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
+
+  fetch(url, {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((google_response) => res.status(200).json({ google_response }))
+    .catch((error) => res.status(500).json({ error }));
 });
 
 app.listen(port, () => {
